4ed6a01eaabdb939c27c4d2ab520380685c90b13
Cam3lnet <contests4game@gmail.com>
2025-04-02 14:01:32 +0100

    Update app.py deleting the clean up mp3 process due hadling problems

--- app.py
+++ app.py
@@ -82,10 +82,6 @@ def download(filename):
         as_attachment=True,
         download_name="converted_audio.mp3"
     )
-    finally:
-        # Clean up uploaded mp3
-        if os.path.exists(mp3_path):
-            os.remove(mp3_path)
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
